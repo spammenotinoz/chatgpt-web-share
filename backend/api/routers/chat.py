7162b8a848066fadd7a8e9196449d4e8d2bd61bd
spammenotinoz <spammenotinoz@gmail.com>
2023-11-10 10:44:03 +1100

    Update chat.py

--- backend/api/routers/chat.py
+++ backend/api/routers/chat.py
@@ -208,10 +208,12 @@ async def check_limits(user: UserReadAdmin, ask_request: AskRequest):
 
     # 判断是否允许使用附件
     if ask_request.openai_web_attachments and len(ask_request.openai_web_attachments) > 0:
-        if ask_request.model != OpenaiWebChatModels.gpt_4_code_interpreter or \
-            ask_request.model != OpenaiWebChatModels.gpt_4 or \
-            config.openai_web.enable_uploading_attachments is False:
-            raise WebsocketInvalidAskException("errors.attachmentsNotAllowed")
+      if (ask_request.model == OpenaiWebChatModels.gpt_4 or \
+          ask_request.model == OpenaiWebChatModels.gpt_4_code_interpreter) and \
+          config.openai_web.enable_uploading_attachments is True:
+        # Allow attachments
+      else:
+        raise WebsocketInvalidAskException("errors.attachmentsNotAllowed")
 
 def check_message(msg: str):
     # 检查消息中的敏感信息
